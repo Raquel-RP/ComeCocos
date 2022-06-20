77ed63e1a9796122e62b0462b35c7856454b821d
Raquel <Raquel@DESKTOP-HD9JBS4>
2022-06-20 21:48:40 +0200

    Comentados el javadoc

--- Comecocos/src/controlador/Controlador.java
+++ Comecocos/src/controlador/Controlador.java
@@ -7,25 +7,39 @@ import modelo.Personaje;
 import vista.ComecocosFrame;
 
 /**
- * Inicializa y genera el juego.
+ * Clase para inicializar, generar el juego en la interfaz gráfica y
+ * traducir eventos y realizar las acciones pertinentes frente a estos.
  *
  * @author Raquel Romero
  * @author Raquel Pulido
  */
 public class Controlador {
 
-    private Modelo modelo_;
-    private ComecocosFrame comecocosFrame_;
+    private Modelo modelo_; //Referencia al objeto modelo
+    private ComecocosFrame comecocosFrame_; //Referencia a la vista
 
+    /**
+     * Contructor de la clase Controlador encargado de establecer el modelo, 
+     * crear un objeto vista, inicializar el juego del modelo para inicializar
+     * el juego y mostrar la vista.
+     * 
+     * @param modelo Modelo a controlar
+     */
     public Controlador(Modelo modelo) {
 
         modelo_ = modelo;
-        comecocosFrame_ = new ComecocosFrame(modelo, this);  // Crear objeto vista (ComecocosFrame)
-        modelo_.inicializarJuego();// Usar el metodo inicializarJuego() del Modelo para inicilizar juego
-        comecocosFrame_.setVisible(true);// Mostrar la vista (comecocosFrame_)
+        comecocosFrame_ = new ComecocosFrame(modelo, this);
+        modelo_.inicializarJuego();
+        comecocosFrame_.setVisible(true);
 
     }
 
+    /**
+     * Capta un evento de teclado y lo traduce asignando la dirección
+     * correspondiente a cada posible tecla pulsada.
+     * 
+     * @param evt Evento de teclado
+     */
     public void teclaPulsadaEnLaberintoPanel(java.awt.event.KeyEvent evt) {
         Comecocos comecocos = modelo_.getComecocos();
         
--- Comecocos/src/vista/ObservadorLaberinto.java
+++ Comecocos/src/vista/ObservadorLaberinto.java
@@ -2,10 +2,16 @@
 package vista;
 
 /**
- *
+ * Interfaz que representa al observador del laberinto llamando a actualizar
+ * el observador del laberinto.
+ * 
  * @author Raquel Romero
  * @author Raquel Pulido
  */
 public interface ObservadorLaberinto {
+    /**
+     * Actualiza el observador laberinto cada vez que haya algún cambio
+     * en el laberinto.
+     */
     void actualizarObservadorLaberinto();
 }
